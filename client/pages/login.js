1c363492405f351ec4aa27ddb72540f911395433
unknown <breldan@sohnandsol.com>
2020-08-06 23:47:01 +0300

    Changes to Login in the Client and Server

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,5 +1,62 @@
+import {useState, useEffect} from 'react';
 import Layout from '../components/Layout';
+import Link  from 'next/link';
+import Router from 'next/router';
+import axios from 'axios';
+import {showSuccessMessage, showErrorMessage} from '../helpers/alerts';
+import {API} from '../config';
 
-const Login = () => <Layout>Login Page</Layout>;
+const Login = () => {
+    const [state, setState] = useState({
+        email: 'breldanmuturi001@gmail.com',
+        password: 'Sunrise@2297',
+        error: '',
+        success: '',
+        buttonText: 'Login'
+    });
+
+    const {email, password, error, success, buttonText} = state;
+
+    const handleChange = (name) => (e) => {
+        setState({...state, [name]: e.target.value, error: '', success: '', buttonText: 'Login' })
+    };
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setState({...state, buttonText: 'Logging in' });
+        try{
+            const response = await axios.post(`${API}/login`, {
+                email, password
+            })
+            console.log(response);
+        } catch (error) {
+            console.log(error);
+            setState({...state, buttonText: 'Login', error: error.response.data.error })
+        }
+    }
+
+    const loginForm = () => (
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <input value={email} onChange={handleChange('email')} type="email" className="form-control" placeholder="Type Your Email"/>
+            </div>
+            <div className="form-group">
+                <input value={password} onChange={handleChange('password')} type="password" className="form-control" placeholder="Type Your Password"/>
+            </div>
+            <div className="form-group">
+                <button className="btn btn-outline-warning">{buttonText}</button>
+            </div>
+        </form>
+    );
+
+    return <Layout>
+          <div className="col-md-6 offset-md-3">
+              <h1>Login</h1>
+                {success && showSuccessMessage(success)}
+                {error && showErrorMessage(error)}
+                {loginForm()}
+          </div>
+    </Layout>;
+};
 
 export default Login;
